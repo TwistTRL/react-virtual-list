de782a655ee26f862f15a739bd7f7a5317ff2fd3
Lingyu Zhou <zly@TRLDSC2.CHBOSTON.ORG>
2019-04-22 19:13:55 -0400

    Updates

---
+++ precache-manifest.1e8ac6fe8b54b8560bdfdcb1c2121572.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "94b0be8371a2b1a7208b",
+    "url": "/react-virtual-list/static/js/runtime~main.292f9453.js"
+  },
+  {
+    "revision": "17ca703d451fdc07678f",
+    "url": "/react-virtual-list/static/js/main.d7afd1ff.chunk.js"
+  },
+  {
+    "revision": "6d58a336ae2e8b12960a",
+    "url": "/react-virtual-list/static/js/2.3053ff1f.chunk.js"
+  },
+  {
+    "revision": "17ca703d451fdc07678f",
+    "url": "/react-virtual-list/static/css/main.49378234.chunk.css"
+  },
+  {
+    "revision": "6d58a336ae2e8b12960a",
+    "url": "/react-virtual-list/static/css/2.b44f1df6.chunk.css"
+  },
+  {
+    "revision": "862a6a9bf9d7fb5575d7c38575dccde3",
+    "url": "/react-virtual-list/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rreact-virtual-list/precache-manifest.9571afa27b954faa91a8f3f8905aeac1.js"
+  "/react-virtual-list/precache-manifest.1e8ac6fe8b54b8560bdfdcb1c2121572.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/rreact-virtual-list/index.html", {
+workbox.routing.registerNavigationRoute("/react-virtual-list/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
